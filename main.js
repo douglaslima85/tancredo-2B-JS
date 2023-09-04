2b9093291d14620de54700cd58b5f3131a0796d0
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2023-09-04 10:47:38 -0300

    atualizando dia 04/09

---
+++ main.js
@@ -0,0 +1,12 @@
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim
+

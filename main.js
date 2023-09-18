dceb8c482af6dd298de10a3809b58dfffd290f49
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2023-09-18 10:42:09 -0300

    AULA DIA 18/09

--- main.js
+++ main.js
@@ -4,21 +4,25 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
 
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
-    const tecla = listaDeTeclas[contador]
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
     
-     const instrumento = tecla.classList[1];
-
-     const idAudio = '#som_${intrumento}'
-
     tecla.onclick = function () {
-        tocaSom ('idAudio');
+        tocaSom(idAudio);
     }
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+        tecla.classList.add('ativa');
+        }
+    }
 
-    console.log(contador);
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
